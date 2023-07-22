839e3585dca16688d48e2e94e6f99fb227872c12
westonludeke <westonludeke@gmail.com>
2023-07-22 16:09:23 -0500

    fix bugs in person.js and requre dotenv in index.js

--- models/person.js
+++ models/person.js
@@ -7,14 +7,13 @@ const url = process.env.MONGO_DB_URL;
 
 console.log('connecting to', url);
 
-mongoose.connect(url);
-
+mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB');
-  });
+  })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message);
-  });
+  })
 
 const personSchema = new mongoose.Schema({
   name: String,
