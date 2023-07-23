092aa5c61defd27cbe29ddb62c55404b422c6bd1
westonludeke <westonludeke@gmail.com>
2023-07-23 13:56:54 -0500

    remove commented out code from index.js and remove objects in persons array

--- index.js
+++ index.js
@@ -29,31 +29,7 @@ app.use(requestLogger);
 app.use(express.static('build'));
 
 let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
 ]
-
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World!</h1>');
-// });
 
 app.get('/api/persons', (request, response) => {
   Person.find({})
@@ -72,13 +48,6 @@ app.get('/api/persons', (request, response) => {
       }
     });
 });
-
-// const generateId = () => {
-//   const maxId = persons.length > 0
-//     ? Math.max(...persons.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// };
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
@@ -110,17 +79,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
   response.status(204).end();
 });
-
-// app.get('/info', (request, response) => {
-//   const numberOfPeople = persons.length;
-//   const currentDate = new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' });
-
-//   const infoMessage = `Phonebook has info for ${numberOfPeople} people.`;
-//   const timestampMessage = `Timestamp: ${currentDate}`;
-
-//   const fullMessage = `${infoMessage} ${timestampMessage}`;
-//   response.send(fullMessage);
-// });
 
 app.use(unknownEndpoint);
 
