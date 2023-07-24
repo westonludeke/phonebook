e6f31c431733bb413d370ea211ec8f0363aab3ac
westonludeke <westonludeke@gmail.com>
2023-07-24 10:42:18 -0500

    update personSchema and post/put validations

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError'){
     return response.status(400).send({ error: 'malformatted id' });
   }
-
+  else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message });
+  }
   next(error);
 }
 
@@ -57,19 +59,18 @@ app.get('/api/persons', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body;
 
-  if (!body.name || !body.number) return response.status(400).json({ error: 'name or number missing' });
-
-  const nameExists = persons.some((person) => person.name === body.name);
-  if (nameExists) return response.status(409).json({ error: 'name must be unique' });
+  // if (!body.name || !body.number) return response.status(400).json({ error: 'name or number missing' });
 
   const person = new Person({
     name: body.name,
     number: body.number
   })
 
-  person.save().then(savedPerson => {
+  person.save()
+  .then(savedPerson => {
     response.json(person);
   })
+  .catch(error => next(error));
 });
 
 app.get('/api/persons/:id', (request, response) => {
@@ -93,15 +94,14 @@ app.delete('/api/persons/:id', (request, response) => {
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson => {
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number},
+    { new: true, runValidators: true, context: 'query' }
+  )
+   .then(updatedPerson => {
       response.json(updatedPerson);
     })
     .catch(error => next(error));
--- mongo.js
+++ mongo.js
@@ -14,8 +14,16 @@ mongoose.set('strictQuery',false);
 mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: { 
+    type: String,
+    minLength: 3,
+    required: true 
+  },
+  number: { 
+    type: String,
+    minLength: 3,
+    required: true 
+  },
 });
 
 const Person = mongoose.model('Person', personSchema);
