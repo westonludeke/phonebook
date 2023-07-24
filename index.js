b70c8546e09b5ab9ac8d9abddbce23f035ea5f85
westonludeke <westonludeke@gmail.com>
2023-07-24 09:52:48 -0500

    add errorHandler, update app.delete, add app.put

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-
-// Define a custom morgan token to log the request body for POST requests
-morgan.token('req-body', (req) => JSON.stringify(req.body));
 
 const Person = require('./models/person');
 
@@ -17,16 +13,25 @@ const requestLogger = (request, response, next) => {
   next();
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 }
 
-// Use the custom morgan token in the format
 app.use(cors());
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
 app.use(requestLogger);
 app.use(express.static('build'));
+
 
 let persons = [
 ]
@@ -76,20 +81,34 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end();
       }
     })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' });
-    })
+    .catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 });
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error));
+})
+
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
