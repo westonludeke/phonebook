ef4f578e12313c98d8e67419cba95ebff2fc298d
westonludeke <westonludeke@gmail.com>
2023-07-25 09:47:34 -0500

    revert index.js

--- index.js
+++ index.js
@@ -1,8 +1,110 @@
-const app = require('./app');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
+const express = require('express');
+const app = express();
 const cors = require('cors');
+require('dotenv').config();
 
+const Person = require('./models/person');
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(express.static('build'));
+
+app.get('/api/persons', (request, response) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons);
+    })
+    .catch(error => {
+      if (error.name === 'MongooseError' && error.message.includes('buffering timed out')) {
+        // Handle timeout error
+        console.error('Timeout error fetching persons:', error);
+        response.status(504).json({ error: 'Timeout Error' });
+      } else {
+        // Handle other errors
+        console.error('Error fetching persons:', error);
+        response.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
+});
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body;
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+  .then(savedPerson => {
+    response.json(savedPerson);
+  })
+  .catch(error => next(error));
+});
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+   .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error));
+})
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
